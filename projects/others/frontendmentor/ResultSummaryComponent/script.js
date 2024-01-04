813b23b72ebe53d9c2921b9cb921288604c8be55
Szeri <szeri323@gmail.com>
2024-01-04 22:43:07 +0100

    clining code for results summary component

--- projects/others/frontendmentor/ResultSummaryComponent/script.js
+++ projects/others/frontendmentor/ResultSummaryComponent/script.js
@@ -1,5 +1,6 @@
 const scores = document.querySelector('.scores');
 let score = ``;
+
 function numberToWords(num) {
     const words = [
         "", "one", "two", "three", "four"
@@ -19,14 +20,10 @@ async function loadJson() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         let data = await response.json();
-        // Teraz możesz użyć zmiennej data, która zawiera dane JSON
-        console.log(data); // wyświetla dane w konsoli
 
-        console.log(data.length);
-
-        for(let i =0; i <data.length; i++)
+        for (let i = 0; i < data.length; i++)
             score += `
-                <div class="score ${numberToWords(i+1)}">
+                <div class="score ${numberToWords(i + 1)}">
                     <img src="${data[i].icon}" alt="">
                     <h2>${data[i].category}</h2>
                     <p><span class="dark">${data[i].score} </span>/ 100</p>
@@ -37,13 +34,8 @@ async function loadJson() {
 
         scores.innerHTML += score;
 
-
-        data.forEach(item => {
-            console.log(item.category);
-        });
-        //console.log(JSON.parse(data));
     } catch (error) {
-        console.error('Błąd przy wczytywaniu JSON:', error);
+        console.error('Error reading JSON:', error);
     }
 }
 
